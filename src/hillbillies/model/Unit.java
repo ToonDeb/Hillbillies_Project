17888e0bfa2d6395f5dc482a6ffe00708139d45c
Natithan <nathan.5005@gmail.com>
2016-02-22 14:24:46 +0100

    getPosition, finished, started setPosition, Exceptions

--- src/hillbillies/model/Unit.java
+++ src/hillbillies/model/Unit.java
@@ -3,10 +3,39 @@ package hillbillies.model;
  * 
  */
 
+import be.kuleuven.cs.som.annotate.*;
+
 /**
- * @author Toon
+ * @authors Toon, Nathan
  * @version 0.1
  */
 public class Unit {
 
+	private double[] position;
+
+	/**
+	 * 
+	 * Returns the position of the unit.
+	 * 
+	 */
+	@Basic
+	public double[] getPosition() {
+		return this.position;
+	}
+	
+	/**
+	 * 
+	 * @param position
+	 */
+	public void setPosition(double[] position) {
+		if (isValidPosition(position))
+			this.position = position;
+		else
+			throw InvalidPositionException(position);
+	}
+	public boolean isValidPosition(double[] position) {
+		return (position[1] >= 0 ) && (position[1] < 50)
+				&& (position[2] >= 0 ) && (position[2] < 50)
+				&& (position[3] >= 0 ) && (position[3] < 50);
+	}
 }
